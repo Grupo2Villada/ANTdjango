b5137873bf02b6268a41ac55a48cd17e6f4271a7
NicolasBazan <nikobazan@hotmail.com>
2018-04-05 13:25:29 -0300

    database v2, almost there

--- config/SistNotas/admin.py
+++ config/SistNotas/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
 
 admin.site.register(Alumno)
-admin.site.register(Curso)
+admin.site.register(Materia)
 admin.site.register(Nota)
 admin.site.register(Profesor)
--- config/SistNotas/models.py
+++ config/SistNotas/models.py
@@ -1,22 +1,6 @@
 from django.db import models
-
+from django.conf import settings
 # Create your models here.
-
-DIVISION_CHOICES = (
-    ('A', 'a'),
-    ('B', 'b'),
-    ('C', 'c'),
-)
-
-YEAR_CHOICES = (
-    ('1', '1°'),
-    ('2', '2°'),
-    ('3', '3°'),
-    ('4', '4°'),
-    ('5', '5°'),
-    ('6', '6°'),
-    ('7', '7°'),
-)
 
 NOTAS_CHOICES = (
     ('1', '1'),
@@ -34,17 +18,30 @@ class Profesor(models.Model):
     nombre = models.CharField(max_length=20)
     apellido = models.CharField(max_length=30)
 
-class Curso(models.Model):
-    year = models.IntegerField(choices=YEAR_CHOICES)
-    division = models.CharField(choices=DIVISION_CHOICES)
-    id_profesor = models.ForeignKeyField(Profesor, on_delete=models.CASCADE, related_name='fk_profesor')
-
-class Nota(models.Model):
-    valor = models.IntegerField(choices=NOTAS_CHOICES)
-    id_alumno = models.ForeignKeyField(Alumno, on_delete=models.CASCADE, related_name='fk_alumno')
-
+    def __str__(self):
+        rta = self.nombre+" "+self.apellido
+        return rta
+        
 class Alumno(models.Model):
     nombre = models.CharField(max_length=20, blank=False, null=False)
     apellido = models.CharField(max_length=30, blank=False, null=False)
-    id_curso = models.ForeignKeyField(Curso, on_delete=models.CASCADE, related_name='fk_curso')
+
+    def __str__(self):
+        rta = self.nombre+" "+self.apellido
+        return rta
+
+class Materia(models.Model):
+    nombre = models.CharField(max_length=50)
+    alumnos = models.ManyToManyField(Alumno, blank=True)
+    profesores = models.ManyToManyField(Profesor, blank=True)
+
+    def __str__(self):
+        return self.nombre
     
+class Nota(models.Model):
+    valor = models.CharField(choices=NOTAS_CHOICES, max_length= 2)
+    materia = models.ForeignKey(Materia, on_delete=models.CASCADE, related_name='fk_materia')
+    alumno = models.ForeignKey(Alumno, on_delete=models.CASCADE, related_name='fk_alumno')
+
+    def __str__(self):
+        return self.valor
